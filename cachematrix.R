414e8a257c189d16d89d7db9518f3d9f3f31e614
Roman Perez <romanperez1433@gmail.com>
2020-09-17 19:33:03 -0500

    First Edition

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## Initialize objects
+        m <- NULL
+        ## Define "behaviors" or functions for objects of type
+        ## makeCacheMatrix
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        ## Define the getter for the matrix "x"
+        get <- function() x
+        setsolve <- function(solve) m<<- solve
+        getsolve <- function() m
+        ## Create a new object by returning a list()
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
@@ -12,4 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data,...)
+        x$setsolve(m)
+        m
 }
